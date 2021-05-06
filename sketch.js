d8a5152f4d7545f6c4f3defd23ec6f835293c3cb
SreeSamhita127 <74597635+SreeSamhita127@users.noreply.github.com>
2021-05-06 21:12:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,8 +9,8 @@ var lastFed
 var fedTime
 
 function preload() {
-  dogImg = loadImage("images/dogImg.png");
-  dogImg1 = loadImage("images/dogImg1.png");
+  dogImg = loadImage("dogImg.png");
+  dogImg1 = loadImage("dogImg1.png");
 }
 
 function setup() {
